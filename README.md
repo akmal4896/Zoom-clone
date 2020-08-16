4ebac02fc9fcf85592e64645a1516624d58307b4
akmal4896 <akmalboss007@email.arizona.edu>
2020-08-16 22:20:09 +0800

    ZOOM CLONE FONAL

---
+++ README.md
@@ -0,0 +1,40 @@
+Description:
+Simple Zoom clone web app.
+
+Notes:
+1) Initialize NodeJS project with npm init with ExpressJS dependencies.
+2) Deploy finished project on Heroku with 
+
+
+=======================================================
+Client:
+1) EJS (JS template engine)
+npm install ejs
+
+2) Fontawesome (Icons eg microphone, video, etc)
+
+3) Bootstrap (css styling)
+
+Backend:
+1) NodeJS with ExpressJS
+npm init
+npm install express
+
+Server Hosting:
+1) Heroku
+npm install -g heroku
+heroku create
+git push heroku master
+
+Database:
+n/a
+
+Others (api, libary, framework):
+1) Socket.io (asycn real time communication; allow client and server make request simultaneously)
+npm install socket.io
+
+2) PeerJS (WebRTC libary for peer-to-peer data/communication between user, browser, apps, mobile, etc)
+npm install peer
+
+3) UUID (to generate unique ID for unique room)
+npm install uuid
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app); //to create http server
+const io = require('socket.io')(server)
+const { v4: uuidv4 } = require('uuid'); //import uuid v4
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
+
+app.set('view engine', 'ejs'); //to set render engine to EJS
+app.use(express.static('public')); //specify the static files location
+app.use('/peerjs', peerServer);
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`); //use string literal to generate uuid
+})
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room});
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', message)
+        })
+    })
+})
+
+server.listen(process.env.PORT || 3030);
